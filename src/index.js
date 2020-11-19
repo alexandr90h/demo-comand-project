d7cb618996c1cc687e2d6be3a01428ca0538c960
Alexandr <63474923+alexandr90h@users.noreply.github.com>
2020-11-19 22:12:24 +0200

    19.11.2020-3

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import {
   fetchMovieByKeyWord,
   fetchMovieFullInfo,
 } from './js/apiService'; 
-
 const refs = {
   inputSeachElem: document.querySelector('.input-film'),
   listBoxElem:document.querySelector('.list-film-conteiner'),
@@ -17,20 +16,43 @@ fetchTopWeekMovie().then(obj => {
   // console.log(obj);
   refs.listBoxElem.insertAdjacentHTML('beforeend', filmListTempl(obj.results));
 });
+ const arrGenres = fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=3f80d4cf4eb52d6e9d2ef400ea3d2acb&language=en-US')
+    .then(r => r.json()).then(obj => {return obj.genres;
+    });
+console.log(arrGenres);
 function onSeach() {
   refs.listBoxElem.innerHTML = '';
   const keyWord = refs.inputSeachElem.value;
   fetchMovieByKeyWord(keyWord).then(obj => {
-  // fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=3f80d4cf4eb52d6e9d2ef400ea3d2acb&language=en-US')
-  //   .then(r => r.json()).then(genre => {
-
-  //   });
-    console.log(obj);
     obj.results.forEach(element => {
-      console.log(element.genre_ids);
-      element.genre_ids.push(111)
+      element.genre_ids.forEach((elem, idx, arr) => {
+        changeGenres(elem, idx, arr);
+      })
     });
     refs.listBoxElem.insertAdjacentHTML('beforeend', filmListTempl(obj.results));
-    console.log(obj);
-  });
+  })
 }
+function changeGenres (elem,idx,arr) {
+          switch (elem) {
+          case 28: arr[idx] = 'Action'; break;
+          case 12: arr[idx] = 'Adventure'; break;
+          case 16: arr[idx] = 'Animation'; break;
+          case 35: arr[idx] = 'Comedy'; break;
+          case 80: arr[idx] = 'Crime'; break;
+          case 99: arr[idx] = 'Documentary'; break;
+          case 18: arr[idx] = 'Drama'; break;
+          case 10751: arr[idx] = 'Family'; break;
+          case 14: arr[idx] = 'Fantasy'; break;
+          case 36: arr[idx] = 'History'; break;
+          case 27: arr[idx] = 'Horror'; break;
+          case 10402: arr[idx] = 'Music'; break;
+          case 9648: arr[idx] = 'Mystery'; break;
+          case 10749: arr[idx] = 'Romance'; break;
+          case 878: arr[idx] = 'Science Fiction'; break;
+          case 10770: arr[idx] = 'TV Movie'; break;
+          case 53: arr[idx] = 'Thriller'; break;
+          case 10752: arr[idx] = 'War'; break;
+          case 37: arr[idx] = 'Western'; break;
+      }
+     return arr[idx];
+    }
